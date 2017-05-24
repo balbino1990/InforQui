158521d1d3a43cb9472772cbb35ae5cf73b182a2
balbino1990 <balbino.pinto@yahoo.com>
2017-05-24 16:01:00 +0100

    Adição a classe Compras_Produtos

---
+++ InforQui_17933/InforQui_17933/Models/Compras_Produtos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace InforQui_17933.Models
+{
+    //Classe Compras Produtos
+    public class Compras_Produtos
+    {
+        //################## Os atributos da tabela Compras_produtos #########
+        //O identificador da tabela Compras_Produtos
+        [Key]
+        [Display(Name = "Identificador de Compras Produtos")]
+        public int Compras_ProdutoID { get; set; }
+        //A quantidade da compra dos produtos
+        [Display(Name = "Quantidade dos Produtos")]
+        [Required(ErrorMessage = "O {0} é preenchimento obrigatorio")]
+        public int Quantidade { get; set; }
+        //O preço dos produtos comprado
+        [Display(Name = "Preço do produto comprados")]
+        [Required(ErrorMessage = "O {0} é preenchimento obrigatorio")]
+        public string Preco { get; set; }
+
+        //######### Relacionamentos e Chaves estrangeiras ###################
+        //chave estrangeira da tabela Compras_Produtos
+        [ForeignKey("Compra")]
+        //existe para criar a FK na BD
+        public int CompraFK { get; set; }
+        //Relacionamento entre a tabela Compra com a tabela Compras_Produtos
+        public Compras Compra { get; set; }
+
+        //chave estrangeira da tabela Produtos
+        [ForeignKey("Produto")]
+        //existe para criar a FK na BD
+        public int ProdutoFK { get; set; }
+        //Relacionamento entre a tabela Produto com a tabela Compras_Produtos
+        public Produtos Produto { get; set; }
+    }
+}
--- InforQui_17933/InforQui_17933/Models/Produtos.cs
+++ InforQui_17933/InforQui_17933/Models/Produtos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,52 @@ namespace InforQui_17933.Models
 {
     public class Produtos
     {
+        //################ O construtor da classe####################
+        public Produtos()
+        {
+            ListaComprasProdutos = new HashSet<Compras_Produtos>();
+        }
+
+        //############ Os atributos da tabela Produtos################
+        //O identificador da tabela Produtos
+        [Key]
+        [Display(Name = "Identifcador do Produto")]
+        public int ProdutoID { get; set; }
+
+        //O nome do produto
+        [Required(ErrorMessage = "O campo {0} é obrigatoria")]
+        [Display(Name = "Nome do produto")]
+        [RegularExpression("[A-ZÁÉÍÓÚ][a-záéíóúàèìòùãõç]+( [A-Z][a-záéíóúàèìòùãõç]+)*",
+                            ErrorMessage = "O campo do {0} não permite o algarismo e letra minúscula no inicio do nome e permite os caracteres português")]
+        public string Nome { get; set; }
+
+        //A descrição do produto
+        [Required(ErrorMessage = "O campo {0} é obrigatoria")]
+        [Display(Name = "Descrição do produto")]
+        [RegularExpression("[A-ZÁÉÍÓÚ0-9][a-záéíóúàèìòùãõç0-9]+( [A-Z][a-záéíóúàèìòùãõç0-9]+)*",
+                          ErrorMessage = "O campo do {0} não permite no ínicio da descrição com letra minúscula")]
+        public string Descricao { get; set; }
+
+        //O preço do produto
+        [Required(ErrorMessage = "O campo {0} é obrigatoria")]
+        [RegularExpression("[€][0-9]+[,]([0-9]{2})",
+                          ErrorMessage = "O preço do produto tem de ser usar a moeda euro (€) e tem de ter dois digitos decimais ultimos")]
+        [Display(Name = "Preço do produto")]
+        public string Preco { get; set; }
+
+        //A imagem do produto
+        //[Required(ErrorMessage = "O campo {0} é obrigatoria")]
+        [Display(Name = "Imagem do produto")]
+        public string Imagem { get; set; }
+
+        //O tipo do produto
+        //[Required(ErrorMessage = "O campo {0} é obrigatoria")]
+        [Display(Name = "Tipo do produto")]
+        public string Tipo { get; set; }
+
+        //########## Relacionamentos e chaves estrangeiras
+        //Relacionamento entre a tabela Produto e Compras_Produtos
+        //1 'Produto' relacionar com muitas 'Compras_Produtos'
+        public virtual ICollection<Compras_Produtos> ListaComprasProdutos { get; set; }
     }
 }
