a75f51ee1000cf9f60dd0185a6c3f482c09f08f9
balbino1990 <balbino.pinto@yahoo.com>
2017-05-25 04:25:41 +0100

    Alteração do RouteConfig

--- InforQui_17933/InforQui_17933/App_Start/RouteConfig.cs
+++ InforQui_17933/InforQui_17933/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace InforQui_17933
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Clientes", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
